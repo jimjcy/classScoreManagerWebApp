0f3ec61a70ba1cc2ff7f67884ecb9677f300fda5
jimjcy <771732203@qq.com>
2025-04-05 21:18:51 +0800

    delete all default codes

---
+++ src/router.js
@@ -0,0 +1,12 @@
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // component: ,
+    }
+  ],
+})
+
+export default router
