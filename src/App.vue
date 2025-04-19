5d3e11c42ad82c9f945fa725095ebcae5847c601
jimjcy123 <771732203@qq.com>
2025-04-19 22:49:49 +0800

    add autoimport

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
-<script setup></script>
+<script setup>
+const navbarStatus = ref("unfold");
 
-<template></template>
+</script>
 
-<style scoped></style>
+<template>
+    <header class="header"><p>test</p>
+    </header>
+    <aside class="navbar" :class="navbarStatus">
+        <p></p>
+    </aside>
+</template>
+
+<style scoped>
+.header {
+    height: 75px;
+    background-color: gray;
+    color: white;
+    position: sticky;
+    top: 0;
+    align-items: center;
+    display: flex;
+}
+</style>
