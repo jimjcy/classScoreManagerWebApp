604a705657ad23c2ac6ff302bbef2b1a98e27c79
jimjcy123 <771732203@qq.com>
2025-04-22 21:36:29 +0800

    navbar test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
-const navbarStatus = ref("unfold");
+const navbarStatus = ref("fold");
 
 </script>
 
 <template>
-    <header class="header"><p>test</p>
+    <header class="header">
+        <button class="menuButton" @click="navbarStatus = navbarStatus === 'fold' ? 'unfold' : 'fold'">菜单</button>
     </header>
-    <aside class="navbar" :class="navbarStatus">
-        <p></p>
+    <aside class="navbar menu" :class="navbarStatus">
+        <p>test</p>
     </aside>
 </template>
 
@@ -20,5 +21,29 @@ const navbarStatus = ref("unfold");
     top: 0;
     align-items: center;
     display: flex;
+}
+.menuButton {
+    height: 100%;
+    width: 90px;
+    border: none;
+    background-color: aliceblue;
+    font-size: 17px;
+    transition: all 0.3s ease-in-out;
+}
+.menuButton:hover {
+    /* background-color: ; */
+}
+.menu {
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    background-color: gray;
+    transition: all 0.5s ease-in-out;
+}
+.fold {
+    width: 0px;
+}
+.unfold {
+    width: 200px;
 }
 </style>
