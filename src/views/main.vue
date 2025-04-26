e9e7c81d7ad93420d0f2e35b3fbb543f089756f6
bingcheng-rain <3267200262@qq.com>
2025-04-26 21:29:15 +0800

    add main page content

--- src/views/main.vue
+++ src/views/main.vue
@@ -3,7 +3,31 @@
 </script>
 <template>
     
+    <form class="form-center" action="">
+        <h1>欢迎来到学生课堂管理系统</h1>
+        <a href="">链接</a>
+        <br>
+        <a href="">链接</a>
+        <br><br>
+    <div>
+        <label for="username">账号:</label>
+        <input type="text" placeholder="请输入账号">
+    </div>
+    <div>
+        <label for="password">密码:</label>
+        <input type="password" placeholder="请输入密码">
+    </div>
+    </form>
+
 </template>
 <style scoped>
-
+   
+    .form-center {
+        width: 300px;
+        text-align: center;    
+    }
+    .button-pos{
+        margin: 10px;
+    }
+    
 </style>
